c1c14e1cf2ee38ca5f0ea97c591f6c05229bb96c
Kompetenzzentrum Barrierefreiheit Volmarstein (KBV) <161310914+kb-esv@users.noreply.github.com>
2024-02-26 15:14:35 +0100

    Initial Code

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# kirby4-a11y
+# Kirby4 a11y
+
 kirbytags for accessibility
+
+ * WCAG 2.2 SC 3.1.2 - Language of Parts
+ * WCAG 2.2 SC 3.1.4 - Abbreviations
+
+## Installation
+
+### Download
+
+Download and copy this repository to `/site/plugins/a11y`.
+
+### Git submodule
+
+```bash
+git submodule add https://github.com/kb-esv/kirby4-a11y.git site/plugins/a11y
+```
+
+### Composer
+
+```bash
+composer require kb-esv/kirby4-a11y
+```
+
+## Usage
+
+Change to an English phrase/text
+
+```
+In England sagen wir: „(lang: en text: It's raining cats and dogs)“
+```
+
+or short
+
+```
+In England sagen wir: „(en: It's raining cats and dogs)“
+```
+
+Change to a German phrase/text
+
+```
+In Germany we say: "(lang: de text: Es regnet wie aus Kübeln)"
+```
+
+or short
+
+```
+In Germany we say: „(de: Es regnet wie aus Kübeln)“
+```
+
+### Additional attributes
+
+You can use the following additional attributes:
+
+- lang: RFC 5646: Tag for Identifying Languages
+- text: Set the text
+- class: Set a class for css
+
+## License
+
+MIT
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * kirby 4 - kirbytags for accessibility
+ * WCAG 2.2 SC 3.1.2 - Language of Parts
+ * WCAG 2.2 SC 3.1.4 - Abbreviations
+ *
+ * @version    0.1.0
+ * @author     Frank Berker <fb@kb-esv.de>, Frank Reins <fr@kb-esv.de>, Birgit Scheer <bs@kb-esv.de>, 
+ * @copyright  Kompetenzzentrum Barrierefreiheit Volmarstein
+ * @link       https://github.com/kb-esv/kirby4-a11y
+ * @license    MIT
+ */
+
+Kirby::plugin('kbv/a11y', [
+    'tags' => [
+        'lang' => [
+            'attr' => [
+                'text',
+                'class'
+            ],
+            'html' => function ($tag) {
+                $lang = $tag->value ?? 'en';
+                $text = $tag->text ?? '';
+                $class = $tag->class ? " class=\"".$tag->class."\"" : '';
+                return "<span lang=\"" . $lang . "\"" . $class . ">" . $text . "</span>";
+            }
+        ],
+        'en' => [
+            'attr' => [
+                'class'
+            ],
+            'html' => function ($tag) {
+                $text = $tag->value ?? '';
+                $class = $tag->class ? " class=\"".$tag->class."\"" : '';
+                return "<span lang=\"en\"" . $class . ">" . $text . "</span>";
+            }
+        ],
+        'de' => [
+            'attr' => [
+                'class'
+            ],
+            'html' => function ($tag) {
+                $text = $tag->value ?? '';
+                $class = $tag->class ? " class=\"".$tag->class."\"" : '';
+                return "<span lang=\"de\"" . $class . ">" . $text . "</span>";
+            }
+        ],
+        'abbr' => [
+            'attr' => [
+                'text',
+                'class',
+                'lang'
+            ],
+            'html' => function ($tag) {
+                $abbr = $tag->value ?? '';
+                $text = $tag->text ?? '';
+                $lang = $tag->lang ? " lang=\"" . $lang . "\"" : '';
+                $class = $tag->class ? " class=\"".$tag->class."\"" : '';
+                return "<abbr title=\"" . $text . "\"" . $lang . $class . ">" . $abbr . "</abbr>";
+            }
+        ],
+        'abk' => [
+            'attr' => [
+                'text',
+                'class',
+            ],
+            'html' => function ($tag) {
+                $abbr = $tag->value ?? '';
+                $text = $tag->text ?? '';
+                $class = $tag->class ? " class=\"".$tag->class."\"" : '';
+                return "<abbr title=\"" . $text . "\"" . $class . ">" . $abbr . "</abbr>";
+            }
+        ]
+    ]
+]);
